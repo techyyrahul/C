fd3c5695083247da9bf444df13f3fe5395e4876b
“[Techy <connectnowwithrahul@gmail.com>
2023-01-31 17:18:13 +0530

    Created folder: Basic Programs, adding the respective basic programs in C language

---
+++ 01_Basic Programs/01.c
@@ -0,0 +1,8 @@
+// Basic program to print "Hello World" in C language
+
+#include<stdio.h>
+
+void main()
+{
+	printf("Hello World");
+}
---
+++ 01_Basic Programs/02.c
@@ -0,0 +1,10 @@
+// Basic program to print text in new line along with some spaces
+
+#include<stdio.h>
+
+void main()
+{
+	printf("Rahul\n");
+	printf("\tIET DAVV\n");
+	printf("\t\tIndore");
+}
